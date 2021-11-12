0102ae52c4bd5a5d34c26f1d7d130df576a4a44f
c8y3 <25362953+c8y3@users.noreply.github.com>
2021-11-12 16:06:11 +0100

    Added test scenario to check an alert is triggered on one message with a (MORE, 0) definition

--- validation/test.py
+++ validation/test.py
@@ -24,25 +24,38 @@ class Test(TestCase):
     def tearDown(self) -> None:
         self._graylog.stop()
 
-    def test_send_alert_should_not_trigger_a_null_pointer_exception(self):
-        self._graylog_rest_api.create_aggregation_count(('MORE', 0), ['port'], period=_PERIOD)
+    def test_send_message_should_not_trigger_a_null_pointer_exception(self):
+        self._graylog_rest_api.create_aggregation_count(('MORE', 0), period=_PERIOD)
         with self._graylog_rest_api.create_gelf_input() as gelf_inputs:
             gelf_inputs.send({})
 
             logs = self._graylog.extract_logs(2*_PERIOD)
             self.assertNotIn('java.lang.NullPointerException', logs)
 
-    def test_send_alert_should_not_raise_exception_when_there_is_a_distinct_field(self):
+    def test_send_message_should_trigger_an_alert(self):
+        self._graylog_rest_api.create_aggregation_count(('MORE', 0), period=_PERIOD)
         with self._graylog_rest_api.create_gelf_input() as gelf_inputs:
-            self._graylog_rest_api.create_aggregation_count(('MORE', 2), ['port'], period=_PERIOD)
+            gelf_inputs.send({})
+            time.sleep(_PERIOD)
+
+            gelf_inputs.send({'short_message': 'pop'})
+            time.sleep(2*_PERIOD)
+            response = self._graylog_rest_api.post('events/search', {})
+            body = response.json()
+
+            self.assertEqual(1, body['total_events'])
+
+    def test_send_message_should_not_raise_exception_when_there_is_a_distinct_field(self):
+        self._graylog_rest_api.create_aggregation_count(('MORE', 2), distinct_fields=['port'], period=_PERIOD)
+        with self._graylog_rest_api.create_gelf_input() as gelf_inputs:
             gelf_inputs.send({'_port': 80})
 
             logs = self._graylog.extract_logs(2*_PERIOD)
             self.assertNotIn('java.lang.IllegalStateException', logs)
 
-    def test_send_alerts_should_trigger_alert_when_there_are_distinct_ports(self):
+    def test_send_messages_should_trigger_alert_when_there_are_distinct_ports(self):
+        self._graylog_rest_api.create_aggregation_count(('MORE', 1), distinct_fields=['port'], period=_PERIOD)
         with self._graylog_rest_api.create_gelf_input() as gelf_inputs:
-            self._graylog_rest_api.create_aggregation_count(('MORE', 1), ['port'], period=_PERIOD)
             gelf_inputs.send({'_port': 80})
             gelf_inputs.send({'_port': 81})
             time.sleep(_PERIOD)
